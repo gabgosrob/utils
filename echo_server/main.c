d99d3290adfca84aa1f75553916d60e2779c6b30
Gabriel Gosselin Roberge <gabriel.gosselin-roberge.1@ulaval.ca>
2025-12-24 02:45:39 -0500

    add udp option (still requires explicit ipv4)

--- echo_server/main.c
+++ echo_server/main.c
@@ -1,5 +1,6 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -36,16 +37,8 @@ void *handle_client_conn(void *client_args)
     return NULL;
 }
 
-int main()
+void tcp_server()
 {
-    // init WSA (windows socket api)
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
-    {
-        printf("Error starting up WSA\n");
-        exit(-1);
-    }
-
     // ipv4, tcp
     SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sock == INVALID_SOCKET)
@@ -59,7 +52,7 @@ int main()
     struct sockaddr_in service;
     service.sin_family = AF_INET;
     service.sin_port = htons(7);
-    inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
+    service.sin_addr.s_addr = htonl(INADDR_ANY);
     if (bind(sock, (SOCKADDR *)&service, sizeof(service)) == SOCKET_ERROR)
     {
         printf("Error during socket bind\n");
@@ -74,7 +67,7 @@ int main()
         printf("Error setting socket to listen\n");
         closesocket(sock);
         WSACleanup();
-        return 1;
+        exit(-1);
     }
 
     // loop for connections
@@ -113,5 +106,99 @@ int main()
     }
 
     WSACleanup();
+}
+
+void udp_server()
+{
+    // ipv4, tcp
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == INVALID_SOCKET)
+    {
+        printf("Error during socket initialization\n");
+        WSACleanup();
+        exit(-1);
+    }
+
+    // bind socket to localhost on port 7
+    struct sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_port = htons(7);
+    service.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sock, (SOCKADDR *)&service, sizeof(service)) == SOCKET_ERROR)
+    {
+        printf("Error during socket bind\n");
+        closesocket(sock);
+        WSACleanup();
+        exit(-1);
+    }
+
+    // loop for connections
+    while (1)
+    {
+        struct sockaddr client_service;
+        int from_len = sizeof(client_service);
+        char buffer[512];
+
+        int len = recvfrom(sock, buffer, sizeof(buffer), 0, &client_service, &from_len);
+        sendto(sock, buffer, len, 0, &client_service, from_len);
+    }
+
+    WSACleanup();
+}
+
+int main(int argc, char *argv[])
+{
+    int option;
+    int flag_t = 0;
+    int flag_u = 0;
+    int both_flags = 0;
+
+    while ((option = getopt(argc, argv, "tu")) != -1)
+    {
+        switch (option)
+        {
+        case 't':
+            flag_t = 1;
+            break;
+        case 'u':
+            flag_u = 1;
+            break;
+        }
+    }
+    both_flags = flag_t && flag_u;
+
+    if (argc - optind != 0)
+    {
+        printf("You must not provide anything other than the -u or -t parameters.\n");
+        exit(-1);
+    }
+    if (!flag_t && !flag_u)
+    {
+        printf("You must specify whether to use TCP (-t) or UDP (-u).\n");
+        exit(-1);
+    }
+    if (both_flags)
+    {
+        printf("Cannot use both TCP and UDP.\n");
+        exit(-1);
+    }
+
+    // init WSA (windows socket api)
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
+    {
+        printf("Error starting up WSA\n");
+        exit(-1);
+    }
+
+    if (flag_t)
+    {
+        tcp_server();
+    }
+    else if (flag_u)
+    {
+        udp_server();
+    }
+
     return 0;
 }
