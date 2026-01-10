b52b6b59fa21265cb8a8cd4e0d795d49e70a0c6f
Gabriel Gosselin Roberge <gabriel.gosselin-roberge.1@ulaval.ca>
2026-01-09 23:28:13 -0500

    cleanup file

--- audio/main.c
+++ audio/main.c
@@ -3,6 +3,39 @@
 #include <math.h>
 #include <stdint.h>
 
+static const uint32_t FREQUENCY_HZ = 44100;
+static const uint32_t DURATION_S = 3;
+static const uint16_t CHANNELS = 2;
+
+void write_header(FILE *file) {
+    uint16_t bits_per_sample = 16;
+    uint16_t bytes_per_block = CHANNELS * bits_per_sample / 8;
+    uint32_t bytes_per_sec = FREQUENCY_HZ * bytes_per_block;
+    uint32_t data_size = bytes_per_sec * DURATION_S;
+    uint32_t file_size = data_size + 44 - 8; // add header and remove 8
+    uint32_t chunk_size = 16;
+    uint16_t data_format = 1; // pcm integer
+
+    // master RIFF chunk
+    fwrite("RIFF", 4, 1, file);
+    fwrite(&file_size, sizeof(file_size), 1, file);
+    fwrite("WAVE", 4, 1, file);
+
+    // format chunk
+    fwrite("fmt ", 4, 1, file);
+    fwrite(&chunk_size, sizeof(chunk_size), 1, file);
+    fwrite(&data_format, sizeof(data_format), 1, file);
+    fwrite(&CHANNELS, sizeof(CHANNELS), 1, file);
+    fwrite(&FREQUENCY_HZ, sizeof(FREQUENCY_HZ), 1, file);
+    fwrite(&bytes_per_sec, sizeof(bytes_per_sec), 1, file);
+    fwrite(&bytes_per_block, sizeof(bytes_per_block), 1, file);
+    fwrite(&bits_per_sample, sizeof(bits_per_sample), 1, file);
+
+    // data chunk
+    fwrite("data", 4, 1, file);
+    fwrite(&data_size, sizeof(data_size), 1, file);
+}
+
 int main() {
     FILE *sound_file = fopen("sound.wav", "wb"); 
     if (sound_file == NULL) {
@@ -10,55 +43,18 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    uint32_t frequency = 44100;
-    uint32_t duration = 3;
-    uint16_t channels = 2;
-    uint16_t bits_per_sample = 16;
-    uint16_t bytes_per_block = channels * bits_per_sample / 8;
-    uint32_t bytes_per_sec = frequency * bytes_per_block;
-    uint32_t data_size = bytes_per_sec * duration;
-    uint32_t file_size = data_size + 44 - 8; // add header and remove 8
-    uint32_t chunk_size = 16;
-    uint16_t data_format = 1; // pcm integer
+    write_header(sound_file);
 
-    // master RIFF chunk
-    fwrite("RIFF", 4, 1, sound_file);
-    fwrite(&file_size, 4, 1, sound_file);
-    fwrite("WAVE", 4, 1, sound_file);
-
-    // format chunk
-    fwrite("fmt ", 4, 1, sound_file);
-    fwrite(&chunk_size, 4, 1, sound_file);
-    fwrite(&data_format, 2, 1, sound_file);
-    fwrite(&channels, 2, 1, sound_file);
-    fwrite(&frequency, 4, 1, sound_file);
-    fwrite(&bytes_per_sec, 4, 1, sound_file);
-    fwrite(&bytes_per_block, 2, 1, sound_file);
-    fwrite(&bits_per_sample, 2, 1, sound_file);
-
-    // sampled data
-    fwrite("data", 4, 1, sound_file);
-    fwrite(&data_size, 4, 1, sound_file);
-    uint32_t frames = frequency*duration;
-    for (uint32_t i = 0; i < frames/2; i++) {
-        float t = (float)i / frequency;
+    // write sampled data
+    uint32_t frames = FREQUENCY_HZ*DURATION_S;
+    for (uint32_t i = 0; i < frames; i++) {
+        float t = (float)i / FREQUENCY_HZ;
         float y = 1.0f * sinf(2.0f * 3.14159265f * 440.0f * t);
         int16_t pcm_integer_y = (int16_t)(INT16_MAX * y);
         // printf("%i\n", pcm_integer_y);
         fwrite(&pcm_integer_y, sizeof(pcm_integer_y), 1, sound_file); // right channel
         fwrite(&pcm_integer_y, sizeof(pcm_integer_y), 1, sound_file); // left channel
     }
-
-    for (uint32_t i = 0; i < frames/2; i++) {
-        float t = (float)i / frequency;
-        float y = 1.0f * sinf(2.0f * 3.14159265f * 261.0f * t);
-        int16_t pcm_integer_y = (int16_t)(INT16_MAX * y);
-        // printf("%i\n", pcm_integer_y);
-        fwrite(&pcm_integer_y, sizeof(pcm_integer_y), 1, sound_file); // right channel
-        fwrite(&pcm_integer_y, sizeof(pcm_integer_y), 1, sound_file); // left channel
-    }
-
-
 
     return EXIT_SUCCESS;
 }
